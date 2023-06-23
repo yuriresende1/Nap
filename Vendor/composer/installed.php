b16f5b8793b90702dd6a5cfeb7a9b6fe8ec32a40
Yuri <112987588+yuriresende1@users.noreply.github.com>
2023-06-23 11:58:00 -0300

    Add files via upload

---
+++ Vendor/composer/installed.php
@@ -0,0 +1,87 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5972a545137109800ffe07d7d037065aa86fcc11',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5972a545137109800ffe07d7d037065aa86fcc11',
+    ),
+    'mpdf/mpdf' => 
+    array (
+      'pretty_version' => 'v8.1.5',
+      'version' => '8.1.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c264ce27af0d794ecd04e201b7e37a06b8a9d720',
+    ),
+    'myclabs/deep-copy' => 
+    array (
+      'pretty_version' => '1.11.1',
+      'version' => '1.11.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7284c22080590fb39f2ffa3e9057f10a4ddd0e0c',
+    ),
+    'paragonie/random_compat' => 
+    array (
+      'pretty_version' => 'v9.99.100',
+      'version' => '9.99.100.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '996434e5492cb4c3edcb9168db6fbb1359ef965a',
+    ),
+    'php-http/message-factory' => 
+    array (
+      'pretty_version' => '1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4d8778e1c7d405cbb471574821c1ff5b68cc8f57',
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.1',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cb6ce4845ce34a8ad9e68117c10ee90a29919eba',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'setasign/fpdi' => 
+    array (
+      'pretty_version' => 'v2.3.7',
+      'version' => '2.3.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bccc892d5fa1f48c43f8ba7db5ed4ba6f30c8c05',
+    ),
+  ),
+);
